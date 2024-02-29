ca5fb04a96b009a77e2b380d907f268eea9a8405
Sean Carroll <macgyversean@gmail.com>
2024-02-29 12:15:49 -0500

    initial commit

---
+++ api/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:*",
+]
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+def home():
+    return {"message: root route."}
